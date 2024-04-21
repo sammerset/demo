bd8afc610dc7ed2619358e5ecd63d7c5b9ef5aad
Kostyantin Pivnik <sammerset@ukr.net>
2024-04-21 16:06:39 +0200

    Fix for reservation validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Reservation < ApplicationRecord
-  validates :title, uniqueness: { scope: %i[check_in check_out] }
+  validates :guest_name, uniqueness: { scope: %i[check_in check_out] }
 end
